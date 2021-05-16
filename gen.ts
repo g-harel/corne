c80f9b650303ec14cf3e9a54af542b6fd6ef2a29
g-harel <gabrielj.harel@gmail.com>
2021-05-16 13:39:42 -0500

    make rendered keys have rounded corners

--- gen.ts
+++ gen.ts
@@ -4,15 +4,50 @@ import {Key, Keyboard, Serial} from "@ijprest/kle-serial";
 import * as glob from "glob";
 import {createCanvas, Canvas} from "canvas";
 
+// TODO svg?
+// TODO rotation
+// TODO padding
+// TODO metadata
+// TODO optional size decoration
+
 const PIXEL_WIDTH = 800;
+
 const KEY_PADDING_TOP = 0.05;
 const KEY_PADDING_SIDE = 0.15;
 const KEY_PADDING_BOTTOM = 0.2;
 
+const PT = 4;
+const PH = PT / 2;
+const PQ = PH / 2;
+const KEY_RADIUS = 0.06;
+
 const DEFAULT_KEY_COLOR = "#cccccc";
 const DEFAULT_STROKE_COLOR = "#000000";
 const DEFAULT_KEY_OVERLAY_COLOR = "#FFFFFF66";
-const DEFAULT_KEY_OVERLAY_STROKE_COLOR = "#00000066";
+const DEFAULT_KEY_OVERLAY_STROKE_COLOR = "#00000022";
+
+// https://stackoverflow.com/a/7838871/3053361
+const renderRoundRect = (
+    ctx: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    w: number,
+    h: number,
+    r: number,
+) => {
+    if (w < 2 * r) r = w / 2;
+    if (h < 2 * r) r = h / 2;
+
+    ctx.beginPath();
+    ctx.moveTo(x + r, y);
+    ctx.arcTo(x + w, y, x + w, y + h, r);
+    ctx.arcTo(x + w, y + h, x, y + h, r);
+    ctx.arcTo(x, y + h, x, y, r);
+    ctx.arcTo(x, y, x + w, y, r);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+};
 
 const renderKey = (
     key: Key,
@@ -30,21 +65,21 @@ const renderKey = (
     const innerH =
         (key.height - KEY_PADDING_TOP - KEY_PADDING_BOTTOM) * unitRatio;
 
-    ctx.strokeStyle = DEFAULT_STROKE_COLOR;
-    ctx.strokeRect(x, y, w, h);
+    const keyRadius = KEY_RADIUS * unitRatio;
 
     ctx.fillStyle = key.color || DEFAULT_KEY_COLOR;
-    ctx.fillRect(x, y, w, h);
+    ctx.lineWidth = PT;
+    ctx.strokeStyle = DEFAULT_STROKE_COLOR;
+    renderRoundRect(ctx, x + PQ, y + PQ, w - PH, h - PH, keyRadius);
 
     ctx.globalCompositeOperation = "overlay";
     ctx.fillStyle = DEFAULT_KEY_OVERLAY_COLOR;
-    ctx.fillRect(innerX, innerY, innerW, innerH);
+    ctx.lineWidth = PT;
     ctx.strokeStyle = DEFAULT_KEY_OVERLAY_STROKE_COLOR;
-    ctx.strokeRect(innerX, innerY, innerW, innerH);
+    renderRoundRect(ctx, innerX, innerY, innerW, innerH, keyRadius);
     ctx.globalCompositeOperation = "source-over";
 };
 
-// TODO rotation
 const renderKeyboard = (keyboard: Keyboard): Canvas => {
     const maxUnits: {x: number; y: number} = keyboard.keys.reduce(
         (max, key) => {
