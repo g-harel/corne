e653e8155470cb05d7ab493f769183f59520a687
g-harel <gabrielj.harel@gmail.com>
2021-05-17 22:19:04 -0500

    add key shine and labels

--- gen.ts
+++ gen.ts
@@ -1,8 +1,8 @@
 import * as fs from "fs";
 
 import {Key, Keyboard, Serial} from "@ijprest/kle-serial";
+import * as c from "color";
 import * as glob from "glob";
-import {createCanvas, Canvas} from "canvas";
 
 // TODO svg?
 // TODO rotation
@@ -10,101 +10,9 @@ import {createCanvas, Canvas} from "canvas";
 // TODO metadata
 // TODO optional size decoration
 
-const PIXEL_WIDTH = 800;
-
-const KEY_PADDING_TOP = 0.05;
-const KEY_PADDING_SIDE = 0.15;
-const KEY_PADDING_BOTTOM = 0.2;
-
-const PT = 4;
-const PH = PT / 2;
-const PQ = PH / 2;
-const KEY_RADIUS = 0.06;
-
-const DEFAULT_KEY_COLOR = "#cccccc";
-const DEFAULT_STROKE_COLOR = "#000000";
-const DEFAULT_KEY_OVERLAY_COLOR = "#FFFFFF66";
-const DEFAULT_KEY_OVERLAY_STROKE_COLOR = "#00000022";
-
-// https://stackoverflow.com/a/7838871/3053361
-const renderRoundRect = (
-    ctx: CanvasRenderingContext2D,
-    x: number,
-    y: number,
-    w: number,
-    h: number,
-    r: number,
-) => {
-    if (w < 2 * r) r = w / 2;
-    if (h < 2 * r) r = h / 2;
-
-    ctx.beginPath();
-    ctx.moveTo(x + r, y);
-    ctx.arcTo(x + w, y, x + w, y + h, r);
-    ctx.arcTo(x + w, y + h, x, y + h, r);
-    ctx.arcTo(x, y + h, x, y, r);
-    ctx.arcTo(x, y, x + w, y, r);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.fill();
-};
-
-const renderKey = (
-    key: Key,
-    ctx: CanvasRenderingContext2D,
-    unitRatio: number,
-) => {
-    const x = key.x * unitRatio;
-    const y = key.y * unitRatio;
-    const w = key.width * unitRatio;
-    const h = key.height * unitRatio;
-
-    const innerX = (key.x + KEY_PADDING_SIDE) * unitRatio;
-    const innerY = (key.y + KEY_PADDING_TOP) * unitRatio;
-    const innerW = (key.width - 2 * KEY_PADDING_SIDE) * unitRatio;
-    const innerH =
-        (key.height - KEY_PADDING_TOP - KEY_PADDING_BOTTOM) * unitRatio;
-
-    const keyRadius = KEY_RADIUS * unitRatio;
-
-    ctx.fillStyle = key.color || DEFAULT_KEY_COLOR;
-    ctx.lineWidth = PT;
-    ctx.strokeStyle = DEFAULT_STROKE_COLOR;
-    renderRoundRect(ctx, x + PQ, y + PQ, w - PH, h - PH, keyRadius);
-
-    ctx.globalCompositeOperation = "overlay";
-    ctx.fillStyle = DEFAULT_KEY_OVERLAY_COLOR;
-    ctx.lineWidth = PT;
-    ctx.strokeStyle = DEFAULT_KEY_OVERLAY_STROKE_COLOR;
-    renderRoundRect(ctx, innerX, innerY, innerW, innerH, keyRadius);
-    ctx.globalCompositeOperation = "source-over";
-};
-
-const renderKeyboard = (keyboard: Keyboard): Canvas => {
-    const maxUnits: {x: number; y: number} = keyboard.keys.reduce(
-        (max, key) => {
-            max.x = Math.max(max.x, key.x + key.width);
-            max.x = Math.max(max.x, key.x + key.x2 + key.width2);
-            max.y = Math.max(max.y, key.y + key.height);
-            max.y = Math.max(max.y, key.y + key.y2 + key.height2);
-            return max;
-        },
-        {x: 0, y: 0},
-    );
-
-    const canvasHeight = (PIXEL_WIDTH * maxUnits.y) / maxUnits.x;
-    const unitRatio = PIXEL_WIDTH / maxUnits.x;
-
-    const canvas = createCanvas(PIXEL_WIDTH, canvasHeight);
-    const ctx = canvas.getContext("2d");
-
-    keyboard.keys.forEach((key) => renderKey(key, ctx, unitRatio));
-
-    return canvas;
-};
-
 export class Element {
     private attributes: Record<string, string | number> = {};
+    private styles: Record<string, string | number> = {};
     private children: any[] = [];
 
     public constructor(public tag: string) {}
@@ -114,8 +22,17 @@ export class Element {
         return this;
     }
 
-    public child(element: Element): Element {
-        this.children.push(element);
+    public style(key: string, value: string | number): Element {
+        this.styles[key] = value;
+        return this;
+    }
+
+    public child(element: Element | string): Element {
+        if (typeof element === "string") {
+            this.children.push({render: () => element});
+        } else {
+            this.children.push(element);
+        }
         return this;
     }
 
@@ -129,9 +46,16 @@ export class Element {
     }
 
     private renderAttributes(): string {
-        const attributes = Object.keys(this.attributes);
-        if (attributes.length === 0) return "";
+        let styleOut = "";
+        const styles = Object.keys(this.styles);
+        for (const style of styles) {
+            styleOut += `${style}:${this.styles[style]};`;
+        }
         let out = "";
+        if (styleOut.length > 0) {
+            out += ` style="${styleOut}"`;
+        }
+        const attributes = Object.keys(this.attributes);
         for (const attribute of attributes) {
             out += ` ${attribute}="${this.attributes[attribute]}"`;
         }
@@ -147,12 +71,23 @@ export class Element {
     }
 }
 
+// Layout
+const PIXEL_WIDTH = 1200;
 const LAYOUT_PADDING = 0.4; // TODO
-const KEY_RADIUS2 = 0.1;
-const KEY_STROKE = "#000000";
-const KEY_STROKE_WIDTH = KEY_RADIUS2 / 2;
+
+// Colors
+const KEY_STROKE_DARKEN = 0.7;
+const KEY_SHINE_DIFF = 0.15;
+
+// Shape
+const KEY_RADIUS = 0.1;
+const KEY_STROKE_WIDTH = 0.015;
+const SHINE_PADDING_TOP = 0.05;
+const SHINE_PADDING_SIDE = 0.12;
+const SHINE_PADDING_BOTTOM = 0.2;
 
 const render2 = (keyboard: Keyboard): string => {
+    // TODO care about rotation.
     const maxUnits: {x: number; y: number} = keyboard.keys.reduce(
         (max, key) => {
             max.x = Math.max(max.x, key.x + key.width);
@@ -169,33 +104,77 @@ const render2 = (keyboard: Keyboard): string => {
         .attr("width", PIXEL_WIDTH)
         .attr("height", (PIXEL_WIDTH * maxUnits.y) / maxUnits.x);
 
-    keyboard.keys.forEach((key) => {
+    keyboard.keys.forEach((k) => {
+        const xRotOrigin = ((k.rotation_x || 0) / maxUnits.x) * 100 + "%";
+        const yRotOrigin = ((k.rotation_y || 0) / maxUnits.y) * 100 + "%";
         parent.child(
-            new Element("rect")
-                .attr(
-                    "style",
-                    `fill:${key.color};stroke:${KEY_STROKE};stroke-width:${KEY_STROKE_WIDTH};`,
+            new Element("circle")
+                .attr("cx", xRotOrigin)
+                .attr("cy", yRotOrigin)
+                .attr("r", 0.05)
+                .attr("fill", k.color),
+        );
+
+        parent.child(
+            new Element("g")
+                .style(
+                    "transform",
+                    `rotate(${k.rotation_angle}deg) translate(${k.x}px, ${k.y}px)`,
                 )
-                .attr("x", key.x)
-                .attr("y", key.y)
-                .attr("rx", KEY_RADIUS2)
-                .attr("width", key.width)
-                .attr("height", key.height),
+                // .style("transform-origin", `${xRotOrigin} ${yRotOrigin}`)
+                // .style("transform-box", "view-box")
+                .child(
+                    new Element("rect")
+                        .style("fill", k.color)
+                        .style(
+                            "stroke",
+                            c(k.color).darken(KEY_STROKE_DARKEN).hex(),
+                        )
+                        .style("stroke-width", KEY_STROKE_WIDTH)
+                        .attr("rx", KEY_RADIUS)
+                        .attr("width", k.width)
+                        .attr("height", k.height),
+                )
+                .child(
+                    new Element("rect")
+                        .style("fill", c(k.color).lighten(KEY_SHINE_DIFF).hex())
+                        .style(
+                            "stroke",
+                            c(k.color).darken(KEY_SHINE_DIFF).hex(),
+                        )
+                        .style("stroke-width", KEY_STROKE_WIDTH)
+                        .attr("x", SHINE_PADDING_SIDE)
+                        .attr("y", SHINE_PADDING_TOP)
+                        .attr("rx", KEY_RADIUS)
+                        .attr("width", k.width - 2 * SHINE_PADDING_SIDE)
+                        .attr(
+                            "height",
+                            k.height - SHINE_PADDING_TOP - SHINE_PADDING_BOTTOM,
+                        ),
+                )
+                .child(
+                    new Element("text")
+                        .style("font-size", 0.2)
+                        .style(
+                            "fill",
+                            c(k.color).darken(KEY_STROKE_DARKEN).hex(),
+                        )
+                        .attr("x", SHINE_PADDING_SIDE)
+                        .attr("y", SHINE_PADDING_TOP)
+                        .child(k.labels.join(",")),
+                ),
         );
     });
 
     return parent.render();
 };
 
-let outFile = glob
+const outFile = glob
     .sync("kle/**/*.json")
-    .map((path) => fs.readFileSync(path).toString())
-    .map((contents) => Serial.parse(contents))
-    .map(renderKeyboard)
-    .reduce((o, c) => o + `<img src="${c.toDataURL("image/png")}" />`, "");
-
-outFile = glob
-    .sync("kle/**/*.json")
+    .map((path) => {
+        console.log(path);
+        return path;
+    })
     .map((path) => fs.readFileSync(path).toString())
     .map((contents) => Serial.parse(contents))
     .map(render2)
